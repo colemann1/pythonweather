c51f9f789cd070d7763fd407b01d4527d0a018ec
colemann1 <colemann1@etsu.edu>
2024-10-11 19:39:32 -0400

    Adding Functionality
    Added weekly and hourly reports, as code was very similar for both.
    
    Currently not working listed in README.md file.
    
    Next update should include: input checking, weather alerts, and precipitation chance.

--- README.md
+++ README.md
@@ -17,6 +17,12 @@ Functions defined, not working:
 - socket
 - sys
 
-## Known Issues:
+## Updates (as of 10/11/2024)
+- Changed naming of variable 'ip' to 'geo' for more consistent naming
+- Added weekly report
+- Added daily (next 24 hours) report (due to similar code to weekly report)
 
-Nothing is working currently except to close program.
+## Known Issues (as of 10/11/2024)
+- No user input checking
+- Alerts currently not implemented
+- Precipitation chance not listed in hourly report
--- main.py
+++ main.py
@@ -4,9 +4,9 @@ import requests
 import socket
 import sys
 
-# Get local user IP address and use this for location information
-ip = geocoder.ip('me')
-geolocation = ip.latlng # coordinates - latitude and longitude - REMOVE WHITE SPACES - Used to format URL for API request
+# Get local user IP address, use this for location information (latlong)
+geo = geocoder.ip('me')
+geolocation = geo.latlng
 
 # Clean geolocation information of whitespace, brackets
 geolocation = str(geolocation)
@@ -14,32 +14,75 @@ geolocation = geolocation.lstrip("[")
 geolocation = geolocation.rstrip("]")
 geolocation = geolocation.replace(" ", "")
 
-locationcity = ip.city
-locationstate = ip.state
+locationcity = geo.city
+locationstate = geo.state
 
 APIString = 'https://api.weather.gov/points/' + geolocation
 
+response = requests.get(APIString)
+
 print(f"\nCurrent Location: ", locationcity, ", ", locationstate, "\n")
 print(f"\nGeolocation: ", geolocation, "\n\n")
-
 
 # Creating menu system
 def display_menu(menu):
     for k, function in menu.items():
         print(k, function.__name__)
 
+
 # Operation for ending session
 def done():
     print("Goodbye")
     sys.exit()
 
+
 # Get daily (next 24 hours) weather report
 def get_daily_report():
-    pass
+    if response.status_code == 200:
+        data = response.json()
+
+    # Extract forecast URL from the response
+    hourly_forecast_url = data['properties']['forecastHourly']
+    
+    print(f"Hourly forecast URL: {hourly_forecast_url}")
+    
+    # Make a request to the hourly forecast URL
+    hourly_forecast_response = requests.get(hourly_forecast_url)
+    
+    if hourly_forecast_response.status_code == 200:
+        hourly_forecast_data = hourly_forecast_response.json()
+        
+        # Print out the forecast
+        print("\nHourly Weather Forecast:\n")
+        for period in hourly_forecast_data['properties']['periods'][:25]: # Limit total output to 24 entries (representing 24 hours from the request time)
+            print(f"{period['startTime']}: {period['temperature']}°F, {period['shortForecast']}, {period['windSpeed']}\n")
+    else:
+        print("Failed to retrieve hourly forecast data.")
+
 
 # Get weekly weather report
 def get_weekly_report():
-    pass
+    if response.status_code == 200:
+        data = response.json()
+
+    # Extract forecast URL from the response
+    forecast_url = data['properties']['forecast']
+    
+    print(f"Forecast URL: {forecast_url}")
+    
+    # Make a request to the 'forecast' URL
+    forecast_response = requests.get(forecast_url)
+    
+    if forecast_response.status_code == 200:
+        forecast_data = forecast_response.json()
+        
+        # Print out the forecast
+        print("\nWeather forecast:\n")
+        for period in forecast_data['properties']['periods']:
+            print(f"{period['name']}: {period['detailedForecast']}\n")
+    else:
+        print("Failed to retrieve forecast data.")
+
 
 # View current alerts
 def get_current_alerts():
@@ -47,6 +90,7 @@ def get_current_alerts():
 
 
 ######
+
 
 # Creates a menu dictionary where the key is a number and the value is a function name
 def main():
